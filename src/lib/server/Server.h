8e5725610470a1a4181f932bbf67dc333df6f2da
sithlord48 <sithlord48@gmail.com>
2025-04-06 23:34:02 -0400

    refactor: port ServerTests to QtTests remove TEST_ENV items from Server

--- src/lib/server/Server.h
+++ src/lib/server/Server.h
@@ -130,16 +130,6 @@ public:
   Server &operator=(Server const &) = delete;
   Server &operator=(Server &&) = delete;
 
-#ifdef TEST_ENV
-  Server() : m_mock(true), m_config(nullptr)
-  {
-  }
-  void setActive(BaseClientProxy *active)
-  {
-    m_active = active;
-  }
-#endif
-
   //! @name manipulators
   //@{
 
